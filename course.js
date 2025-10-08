af064c8ea742c9ff549d7f8207ca4ca9d347e6b0
Kayz-ee <lildhain18@gmail.com>
2025-10-08 12:14:07 +0100

    Create course.js

---
+++ course.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  color: {
+    type: String,
+    default: '#001F54'
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Course', courseSchema);
